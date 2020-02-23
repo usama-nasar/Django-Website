4a3671bbaeed2fea864c155d118d6783949e17b8
usama-nasar <usama_4000@yahoo.com>
2020-02-24 04:14:59 +1100

    Add files via upload

---
+++ mysite/main/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from tinymce.widgets import TinyMCE
+from django.db import models
+# Register your models here.
+from .models import Tutorial, TutorialSeries, TutorialCategory
+
+class TutorialAdmin(admin.ModelAdmin):
+
+	fieldsets = [
+		("Title/date", {"fields":["tutorial_title", "tutorial_published"]}),
+		("URL", {"fields":["tutorial_slug"]}),
+		("Series", {"fields":["tutorial_series"]}),
+		("Content", {"fields":["tutorial_content"]})
+	]
+
+	formfield_overrides = {
+		models.TextField: {'widget': TinyMCE()}
+	}
+
+admin.site.register(TutorialSeries)
+
+admin.site.register(TutorialCategory)
+
+admin.site.register(Tutorial, TutorialAdmin)
